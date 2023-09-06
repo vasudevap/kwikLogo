d222060a67b10c52665da61fe213f9fbdfb20d54
PV <140992905+vasudevap@users.noreply.github.com>
2023-09-06 17:58:37 -0400

    starter code and files structure

---
+++ index.js
@@ -0,0 +1,72 @@
+// Import inquirer class to prompt user
+const inquirer = require('inquirer');
+// Import file system package to access writing and reading local files.
+const fs = require('fs');
+
+// Array of questions for user input
+const userPrompts = [
+    'Enter the letter/letters that would go on the logo (at most 3 letters)',
+    'Enter a color for the text (keyword or a hexadecimal number)',
+    'Enter a shape (select one below):',
+    'Enter a color for the shape (color keyword or a hexadecimal number)'
+];
+
+const shapeChoices = [
+    'circle',
+    'triangle',
+    'square'
+];
+
+// Function to write SVG file
+function createLogoSVG(logoData) {
+
+    fs.appendFile('./Logo.svg', logoData+"\n", (err) => err ? console.error(err) : "");
+
+}
+
+// Function to initialize the app
+function init() {
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "letters",
+            //'1. Enter the letter/letters that would go on the logo (at most 3 letters)', 
+            message: userPrompts[0],
+        },
+        {
+            type: "input",
+            name: "letterColor",
+            //'2. Enter a color for the text (keyword or a hexadecimal number)'
+            message: userPrompts[1],
+        },
+        {
+            type: "list",
+            name: "shape",
+            //'3. Enter a shape (select one below):'
+            message: userPrompts[2],
+            choices: shapeChoices
+        },
+        {
+            type: "input",
+            name: "shapeColor",
+            //'4. Enter a color for the shape (color keyword or a hexadecimal number)'
+ 
+            message: userPrompts[3],
+        },
+    ])
+    .then((answers) => {
+
+        //use generateMarkdown to populate the template using answers
+        //use writeReadme to write the populated template to the local file
+        createLogoSVG(generateSVG(answers));
+        
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            console.log("Prompt couldn't be rendered in the current environment");
+        } else {
+            console.log("Something else went wrong");
+        }
+    });
+}
