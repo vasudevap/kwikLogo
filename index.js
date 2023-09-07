6a8abec8cf89fed6b02a54fcc9905361bfe61e19
PV <140992905+vasudevap@users.noreply.github.com>
2023-09-07 19:42:47 -0400

    added: restart post text validation

--- index.js
+++ index.js
@@ -28,58 +28,60 @@ function createLogoSVG(logoData) {
 // Function to initialize the app
 function init() {
     inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "letters",
-            // Regular expression that input must be valid against.
+        .prompt([
+            {
+                type: "input",
+                name: "letters",
+                // Regular expression that input must be valid against.
 
-            //'1. Enter the letter/letters that would go on the logo (at most 3 letters)', 
-            message: userPrompts[0],
-        },
-        {
-            type: "input",
-            name: "letterColor",
-            //'2. Enter a color for the text (keyword or a hexadecimal number)'
-            message: userPrompts[1],
-        },
-        {
-            type: "list",
-            name: "shape",
-            //'3. Enter a shape (select one below):'
-            message: userPrompts[2],
-            choices: shapeChoices
-        },
-        {
-            type: "input",
-            name: "shapeColor",
-            //'4. Enter a color for the shape (color keyword or a hexadecimal number)'
- 
-            message: userPrompts[3],
-        },
-    ])
-    .then((answers) => {
+                //'1. Enter the letter/letters that would go on the logo (at most 3 letters)', 
+                message: userPrompts[0],
+            },
+            {
+                type: "input",
+                name: "letterColor",
+                //'2. Enter a color for the text (keyword or a hexadecimal number)'
+                message: userPrompts[1],
+            },
+            {
+                type: "list",
+                name: "shape",
+                //'3. Enter a shape (select one below):'
+                message: userPrompts[2],
+                choices: shapeChoices
+            },
+            {
+                type: "input",
+                name: "shapeColor",
+                //'4. Enter a color for the shape (color keyword or a hexadecimal number)'
 
-        // with the answers we get back
-        // perform some validation:
-        //
-        // 1 - check to see if call function to create the file in the local folder
-        //
-        // if(answers.letters.length > 3)
-        if(answers.letters.length>3){
-            console.log("For best results, please use three letters at a maximum!");
-        } else {
-            createLogoSVG(generateSVG(answers));
-        }
-        init();
-    })
-    .catch((error) => {
-        if (error.isTtyError) {
-            console.log("Prompt couldn't be rendered in the current environment");
-        } else {
-            console.log(error);
-        }
-    });
+                message: userPrompts[3],
+            },
+        ])
+        .then((answers) => {
+
+            // with the answers we get back
+            // perform some validation:
+            //
+            // 1 - check to see if call function to create the file in the local folder
+            //
+            // if(answers.letters.length > 3)
+            if (answers.letters.length > 3) {
+                console.log("For best results, please use three letters at a maximum!");
+                console.log("\nPlease try again\n");
+                init();
+            } else {
+                createLogoSVG(generateSVG(answers));
+            }
+
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                console.log("Prompt couldn't be rendered in the current environment");
+            } else {
+                console.log(error);
+            }
+        });
 }
 
 // Function call to initialize app
